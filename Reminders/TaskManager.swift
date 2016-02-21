7a48eb321ad04f9159866241a1b7c61de47054dd
Ryan Aubrey <rma7qb@labmini05.lab.cs.virginia.edu>
2016-02-20 22:35:41 -0500

    first commit

---
+++ Reminders/TaskManager.swift
@@ -0,0 +1,13 @@
+//
+//  TaskManager.swift
+//  Reminders
+//
+//  Created by Ryan Aubrey on 2/20/16.
+//  Copyright © 2016 Ryan Aubrey. All rights reserved.
+//
+
+import Cocoa
+
+class TaskManager: UITableViewController {
+
+}
